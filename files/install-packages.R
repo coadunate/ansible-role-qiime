dcb74c05cb0683a44eeb1bef83833ffa74b7c383
Tanner Dowhy <tad638@mail.usask.ca>
2017-07-14 14:15:36 -0600

    Tested and workd

---
+++ files/install-packages.R
@@ -0,0 +1,3 @@
+install.packages(c('ape', 'biom', 'optparse', 'RColorBrewer', 'randomForest', 'vegan'), repos = "http://cran.us.r-project.org")
+source('http://bioconductor.org/biocLite.R')
+biocLite(c('DESeq2', 'metagenomeSeq'))
